3867972c7ac88632777c68ec8e72a1aac3eb8f46
zp <zhangpeng@ireadercity.com>
2019-03-18 19:05:19 +0800

    -

--- app/src/main/java/com/vanzh/wlha_kt/net/NetHelper.kt
+++ app/src/main/java/com/vanzh/wlha_kt/net/NetHelper.kt
@@ -5,6 +5,12 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.cert.X509Certificate
+import com.vanzh.wlha_kt.net.NetHelper.TrustAllManager
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import javax.net.ssl.*
+
 
 /**
  * created by zp on 2019/3/18.
@@ -35,6 +41,44 @@ class NetHelper {
         return OkHttpClient.Builder()
             .retryOnConnectionFailure(true)
             .addInterceptor(LogInterceptor())
+            .sslSocketFactory(createSSLSocketFactory(),TrustAllManager())
             .build()
+    }
+
+    private fun createSSLSocketFactory(): SSLSocketFactory? {
+
+        var sSLSocketFactory: SSLSocketFactory? = null
+
+        try {
+            val sc = SSLContext.getInstance("TLS")
+            sc.init(
+                null, arrayOf<TrustManager>(TrustAllManager()),
+                SecureRandom()
+            )
+            sSLSocketFactory = sc.getSocketFactory()
+        } catch (e: Exception) {
+        }
+
+        return sSLSocketFactory
+    }
+
+    class TrustAllManager : X509TrustManager {
+        override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+            throw  CertificateException()
+        }
+
+        override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+            throw CertificateException()
+        }
+
+        override fun getAcceptedIssuers(): Array<X509Certificate>? {
+            return null
+        }
+    }
+
+    private class TrustAllHostnameVerifier : HostnameVerifier {
+        override fun verify(hostname: String, session: SSLSession): Boolean {
+            return true
+        }
     }
 }
